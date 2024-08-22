8ae8a2708daa5af9af09427bf40caaccf6469994
Daniel Reuter <daniel@steadybit.com>
2024-08-22 08:34:40 +0200

    chore: updated examples

--- example/cdk/lib/failure_lambda_example-stack.ts
+++ example/cdk/lib/failure_lambda_example-stack.ts
@@ -1,18 +1,24 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as ssm from "aws-cdk-lib/aws-ssm";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+
+import { Stack, StackProps } from "aws-cdk-lib";
+
+import { Construct } from "constructs";
 
 export class FailureLambdaExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const failureLambdaParameter = new ssm.StringParameter(this, "failureLambdaParameter", {
-        stringValue: '{"isEnabled": false, "failureMode": "latency", "rate": 1, "minLatency": 100, "maxLatency": 400, "exceptionMsg": "Exception message!", "statusCode": 404, "diskSpace": 100, "denylist": ["s3.*.amazonaws.com", "dynamodb.*.amazonaws.com"]}',
+    const failureLambdaParameter = new ssm.StringParameter(
+      this,
+      "failureLambdaParameter",
+      {
+        stringValue:
+          '{"isEnabled": false, "failureMode": "latency", "rate": 1, "minLatency": 100, "maxLatency": 400, "exceptionMsg": "Exception message!", "statusCode": 404, "diskSpace": 100, "denylist": ["s3.*.amazonaws.com", "dynamodb.*.amazonaws.com"]}',
       }
     );
     const failureLambdaTable = new dynamodb.Table(this, "failureLambdaTable", {
@@ -21,8 +27,11 @@ export class FailureLambdaExampleStack extends Stack {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
 
-    const failureLambdaFunction = new lambdaNode.NodejsFunction(this, "failureLambdaFunction", {
-        runtime: lambda.Runtime.NODEJS_14_X,
+    const failureLambdaFunction = new lambdaNode.NodejsFunction(
+      this,
+      "failureLambdaFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
         handler: "handler",
         entry: `${__dirname}/resources/index.js`,
         environment: {
@@ -30,7 +39,7 @@ export class FailureLambdaExampleStack extends Stack {
           FAILURE_INJECTION_TABLE: failureLambdaTable.tableName,
         },
         bundling: {
-          nodeModules: ["failure-lambda"],
+          nodeModules: ["@steadybit/failure-lambda"],
         },
       }
     );
@@ -38,9 +47,8 @@ export class FailureLambdaExampleStack extends Stack {
     new apigateway.LambdaRestApi(this, "failureLambdaApi", {
       handler: failureLambdaFunction,
     });
-    
+
     failureLambdaParameter.grantRead(failureLambdaFunction);
     failureLambdaTable.grantWriteData(failureLambdaFunction);
-
   }
 }
